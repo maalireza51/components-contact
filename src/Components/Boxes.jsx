f057bd0167eeb3c389f4541eb4218d2723d3e690
maalireza51 <maalireza51@yahoo.com>
2022-04-02 22:58:57 +0430

    complete toggles

--- src/Components/Boxes.jsx
+++ src/Components/Boxes.jsx
@@ -3,36 +3,64 @@ import React, { Component } from 'react'
 export default class Boxes extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            boxes: [
+                { id: 1, title: 'box1' },
+                { id: 2, title: 'box2' },
+                { id: 3, title: 'box3' },
+                { id: 4, title: 'box4' },
+            ],
+            activeBoxes: [1,2, 3, 4]
+        }
         this.handleChange = this.handleChange.bind(this);
     }
-    handleChange({ target }) {
-        if (target.checked) {
-            // target.removeAttribute('checked');
-            target.setAttribute('checked', false);
-        } else {
-            target.setAttribute('checked', true);
+    handleChange({target},id) {
+        const activeBoxes = [...this.state.activeBoxes];
+        if(activeBoxes.includes(id)){
+            activeBoxes.splice(activeBoxes.indexOf(id),1);
+        }else{
+            activeBoxes.push(id);
         }
+        this.setState(state=>{
+            return{
+                ...state,
+                activeBoxes
+            }
+        })
     }
     render() {
+        const toggles = this.state.boxes.map(box => {
+            return (
+                <div className="toggle-button-cover">
+                    <div className="button-cover">
+                        <div className="button b2" id="button">
+                            <input type="checkbox" className="checkbox" onClick={(e) => this.handleChange(e,box.id)} defaultChecked={!this.state.activeBoxes.includes(box.id)} />
+                            <div className="knobs">
+                                <span></span>
+                            </div>
+                            <div className="layer"></div>
+                        </div>
+                    </div>
+                </div>
+            )
+        });
+        const boxes = this.state.boxes.map(box => {
+            if (this.state.activeBoxes.includes(box.id)) {
+                return (
+                    <div className="box">
+                        {box.title}
+                    </div>
+                )
+            }
+        })
+
         return (
             <>
                 <aside>
-                    <div className="box">
-                        box1
-                    </div>
+                    {boxes}
                 </aside>
                 <main>
-                    <div className="toggle-button-cover">
-                        <div className="button-cover">
-                            <div className="button b2" id="button">
-                                <input type="checkbox" className="checkbox" onClick={this.handleChange} defaultChecked={this.props.complete} />
-                                <div className="knobs">
-                                    <span></span>
-                                </div>
-                                <div className="layer"></div>
-                            </div>
-                        </div>
-                    </div>
+                    {toggles}
                 </main>
             </>
         )
